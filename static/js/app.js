75e833a6e0cebab3f9513034ba23b49255e940e8
餔餔 <52117569+willhwh@users.noreply.github.com>
2020-10-01 21:27:55 -0500

    improve the id list

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,22 @@
 // id list
-var names=["940", "941", "943", "944", "945", "946", "947", "948", "949", "950", "952", "953", "954", "955", "956", "958", "959", "960", "961", "962", "963", "964", "966", "967", "968", "969", "970", "971", "972", "973", "974", "975", "978", "1233", "1234", "1235", "1236", "1237", "1238", "1242", "1243", "1246", "1253", "1254", "1258", "1259", "1260", "1264", "1265", "1273", "1275", "1276", "1277", "1278", "1279", "1280", "1281", "1282", "1283", "1284", "1285", "1286", "1287", "1288", "1289", "1290", "1291", "1292", "1293", "1294", "1295", "1296", "1297", "1298", "1308", "1309", "1310", "1374", "1415", "1439", "1441", "1443", "1486", "1487", "1489", "1490", "1491", "1494", "1495", "1497", "1499", "1500", "1501", "1502", "1503", "1504", "1505", "1506", "1507", "1508", "1510", "1511", "1512", "1513", "1514", "1515", "1516", "1517", "1518", "1519", "1521", "1524", "1526", "1527", "1530", "1531", "1532", "1533", "1534", "1535", "1536", "1537", "1539", "1540", "1541", "1542", "1543", "1544", "1545", "1546", "1547", "1548", "1549", "1550", "1551", "1552", "1553", "1554", "1555", "1556", "1557", "1558", "1561", "1562", "1563", "1564", "1572", "1573", "1574", "1576", "1577", "1581", "1601"];
+//var names=["940", "941", "943", "944", "945", "946", "947", "948", "949", "950", "952", "953", "954", "955", "956", "958", "959", "960", "961", "962", "963", "964", "966", "967", "968", "969", "970", "971", "972", "973", "974", "975", "978", "1233", "1234", "1235", "1236", "1237", "1238", "1242", "1243", "1246", "1253", "1254", "1258", "1259", "1260", "1264", "1265", "1273", "1275", "1276", "1277", "1278", "1279", "1280", "1281", "1282", "1283", "1284", "1285", "1286", "1287", "1288", "1289", "1290", "1291", "1292", "1293", "1294", "1295", "1296", "1297", "1298", "1308", "1309", "1310", "1374", "1415", "1439", "1441", "1443", "1486", "1487", "1489", "1490", "1491", "1494", "1495", "1497", "1499", "1500", "1501", "1502", "1503", "1504", "1505", "1506", "1507", "1508", "1510", "1511", "1512", "1513", "1514", "1515", "1516", "1517", "1518", "1519", "1521", "1524", "1526", "1527", "1530", "1531", "1532", "1533", "1534", "1535", "1536", "1537", "1539", "1540", "1541", "1542", "1543", "1544", "1545", "1546", "1547", "1548", "1549", "1550", "1551", "1552", "1553", "1554", "1555", "1556", "1557", "1558", "1561", "1562", "1563", "1564", "1572", "1573", "1574", "1576", "1577", "1581", "1601"];
+d3.json("samples.json").then(function(data) {
+    var names=data.names;
+    // dropdown list position
+    var dropDownList = d3.select('#selDataset');
+    //
+    names.forEach(function(name){
+        dropDownList.append('option')
+            .text(name)
+            .attr('value',name);
+    //console.log(names);
+    });
+});
 
 
-// dropdown list position
-var dropDownList = d3.select('#selDataset');
 
-// dropdown list value input
-for (i = 0; i < names.length; i++) {
-    dropDownList.append('option')
-        .text(names[i])
-        .attr('value',names[i]);
-};
+
+
 
 //----event handler
 d3.select('#selDataset').on('change',getDropDownValue);
@@ -40,21 +46,26 @@ function getDropDownValue(){
                 // console.log(metaWfreq)
 
                 //----- info section
-                var infoSection = d3.select('#sample-metadata')
-                infoSection.html("")
-
-                infoSection.append('h5')
-                    .text(`id: ${metaDataId}`)
-                infoSection.append('h5')
-                    .text(`ethnicity: ${metaEthnicity}`)
-                infoSection.append('h5')
-                    .text(`gender: ${metaGender}`)
-                infoSection.append('h5')
-                    .text(`location: ${metaLocation}`)
-                infoSection.append('h5')
-                    .text(`bbtype: ${metaBbtype}`)
-                infoSection.append('h5')
-                    .text(`wfreq: ${metaWfreq}`)
+                var infoSection = d3.select('#sample-metadata');
+                infoSection.html("");
+                Object.entries(data).forEach(([key,value])=>{
+                    var temp =`${key}: ${value}`
+                    infoSection.append('h5')
+                        .text(temp)
+                    console.log(temp)
+                });
+                // infoSection.append('h5')
+                //     .text(`id: ${metaDataId}`)
+                // infoSection.append('h5')
+                //     .text(`ethnicity: ${metaEthnicity}`)
+                // infoSection.append('h5')
+                //     .text(`gender: ${metaGender}`)
+                // infoSection.append('h5')
+                //     .text(`location: ${metaLocation}`)
+                // infoSection.append('h5')
+                //     .text(`bbtype: ${metaBbtype}`)
+                // infoSection.append('h5')
+                //     .text(`wfreq: ${metaWfreq}`)
                 //----- info section finish
 
                 
@@ -69,6 +80,7 @@ function getDropDownValue(){
                         mode: "gauge+number",
                         gauge: {
                             axis: { range: [null, 9]},
+                            //labels: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
                             steps: [
                                 { range: [0, 1], color: "#ffffe0" },
                                 { range: [1, 2], color: "#c5eddf" },
@@ -136,8 +148,8 @@ function getDropDownValue(){
                 // console.log('bubble chart match')
                 var sampleOtuIds_bubble = data.otu_ids
                 var sampleValues_bubble = data.sample_values
-                // console.log(sampleOtuIds_bubble)
-                // console.log(sampleValues_bubble)
+                console.log(sampleOtuIds_bubble)
+                console.log(sampleValues_bubble)
                 var trace2={
                     x:sampleOtuIds_bubble,
                     y:sampleValues_bubble,
@@ -149,7 +161,11 @@ function getDropDownValue(){
                     text:sampleLabels
                 }
                 var data2=[trace2]
-                Plotly.newPlot('bubble',data2)
+                var layout2={
+                    xaxis:{title:'OTU ID'}
+                }
+
+                Plotly.newPlot('bubble',data2,layout2)
                 //-----bubble chart section finish
 
             }
